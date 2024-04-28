462c3941c4b180cab26b348909c136c7cbf173f2
hakuturu583 <ms.kataoka@gmail.com>
2024-04-29 01:46:29 +0900

    add test case
    Signed-off-by: hakuturu583 <ms.kataoka@gmail.com>

--- test/test_type_adapter.cpp
+++ test/test_type_adapter.cpp
@@ -14,6 +14,46 @@
 
 #include <gtest/gtest.h>
 
+#include <message_synchronizer/message_synchronizer.hpp>
+#include <pcl_type_adapter/pcl_type_adapter.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <string>
+
+class NodeWithAdapter : public rclcpp::Node
+{
+public:
+  explicit NodeWithAdapter(const rclcpp::NodeOptions & option)
+  : Node("example", option),
+    sync_(
+      this,
+      {"/perception/front_lidar/points_transform_node/output",
+       "/perception/rear_lidar/points_transform_node/output"},
+      std::chrono::milliseconds{100}, std::chrono::milliseconds{30})
+  {
+    // const auto func =
+    //   std::bind(&Example::callback, this, std::placeholders::_1, std::placeholders::_2);
+    // sync_.registerCallback(func);
+  }
+
+private:
+  message_synchronizer::MessageSynchronizer2<
+    sensor_msgs::msg::PointCloud2, sensor_msgs::msg::PointCloud2>
+    sync_;
+  //   void callback(
+  //     const std::optional<sensor_msgs::msg::PointCloud2> & msg0,
+  //     const std::optional<sensor_msgs::msg::PointCloud2> & msg1)
+  //   {
+  //     if (msg0) {
+  //       std::cout << __FILE__ << "," << __LINE__ << std::endl;
+  //     }
+  //     if (msg1) {
+  //       std::cout << __FILE__ << "," << __LINE__ << std::endl;
+  //     }
+  //     std::cout << __FILE__ << "," << __LINE__ << std::endl;
+  //   }
+};
+
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
